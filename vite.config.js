c0c9c7c7650104cfae54f2165eccd88b7cac1db1
noumek4 <noumek.aaron@giortech.com>
2026-06-12 12:30:26 +0100

    added stuufs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Mooves-Travel/',
   plugins: [react()],  server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
